a56d0da47f15e931d566e477d6ab70ef3cfe724a
Raj kc <157774717+rajkc45@users.noreply.github.com>
2026-05-22 15:26:15 +0545

    created todo with add functionality and stats for total and completed todos

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 let todo =[]//empty array
+let completed=0;
 function getvalue(){
     let value= document.getElementById("todoInput").value;// box bata value lera ako
     console.log(value);
+    const newtodo ={
+        id: Math.floor(Math.random()*1000),
+        text: value,
+        completed: false,
+    
+    };
+    todo.push(newtodo);
 
     let messages= document.getElementById("todos"); //pront garana ho k
     console.log(messages);
 
-    todo.push(value);// click vayesi array ma add
+    // todo.push(value);// click vayesi array ma add
 
-    const todoList = todo.map((todo) => `<li>${todo}</li>`).join(""); // line by line garauna ko lagi.ani join chai comma hatauna rakheko
+    const todoList = todo.map((todo) => `<li>${todo.text}</li> <input type="checkbox" onchange="check(this)">Completed`).join("");
+
     messages.innerHTML = todoList;
-    todo.length;
+    document.getElementById("total").innerHTML = todo.length;
+}
+    
+function check(element){
+    console.log(element);
+  if (element.checked){
+        completed=completed+1;
+        document.getElementById("completed").innerHTML = completed;
+    }else {
+    completed=completed-1;
+        document.getElementById("completed").innerHTML = completed;
+ 
+console.log(element.checked);
+}
 }
